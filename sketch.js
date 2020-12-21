27967e222b3ff825d7ea49f7254e81e75fcdd0d3
SHUBHAM1234A <74237720+SHUBHAM1234A@users.noreply.github.com>
2020-12-21 19:43:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+//namespacing
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine,world;
+var ground;
+var box1, box2;
+
+function setup() {
+  var canvas = createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(200,390,400,20);
+  box1 = new Box(200,200,50,50);
+  box2 = new Box(225,120,50,80);
+  
+}
+
+function draw() {
+  background("lightblue");  
+  Engine.update(engine);
+  
+  ground.display();
+  box1.display();
+  box2.display();
+
+
+  
+}
